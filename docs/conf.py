a3d3f28f2d1fe3d485bd9adca271abe59504bf54
xdssio <jonathan@xdss.io>
2023-04-21 15:17:58 +0200

    adjust book-theme

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,8 @@ release = '0.0.1a'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.viewcode", "sphinx_rtd_theme"]
+extensions = ["myst_parser", "sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.viewcode", "sphinx_rtd_theme",
+              "sphinx_book_theme"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'docs', '.venv']
@@ -22,7 +23,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'docs', '.venv']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_pydata_theme'
+html_theme = "sphinx_book_theme"
+
 html_static_path = ['_static']
 
 import os

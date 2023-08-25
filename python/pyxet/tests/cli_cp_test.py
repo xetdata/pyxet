1492c90ed9890d9e6c025eac801c84595c1c5b96
Di <di@xetdata.com>
2023-08-26 01:05:43 +0800

    fix test

--- python/pyxet/tests/cli_cp_test.py
+++ python/pyxet/tests/cli_cp_test.py
@@ -1,5 +1,6 @@
 import pyxet
 import utils
+import shutil
 import tempfile
 
 
@@ -8,38 +9,38 @@ def test_single_file_upload():
     repo = utils.test_repo()
     b1 = utils.new_random_branch_from(f"xet://{user}/{repo}", "main")
     
-    # generate a random file in a temp dir
-    dir = tempfile.mkdtemp()
-    local_file = f"{dir}/data"
-    utils.random_binary_file(local_file, 1024)
-
-    # test variations of path
-    source_list = [
-        f"{dir}/data",
-        f"{dir}/*"
-    ]
-
-    dest_list = [
-        # (dest in cp command, expected path of remote file)
-        (f"xet://{user}/{repo}/{b1}", f"xet://{user}/{repo}/{b1}/data"),
-        (f"xet://{user}/{repo}/{b1}/", f"xet://{user}/{repo}/{b1}/data"),
-        (f"xet://{user}/{repo}/{b1}/zz", f"xet://{user}/{repo}/{b1}/zz"),
-    ]
-
-    recursive_list = [
-        False,
-        True,
-    ]
-
     try:
-        for src in source_list:
-            for dest in dest_list:
-                for r in recursive_list:
-                    pyxet.cli._root_copy(src, dest[0], "add data", r)
-                    utils.assert_remote_files_exist(dest[1], [dest[1][6:]])
-                    pyxet.PyxetCLI.rm(dest[1])
+        # generate a random file in a temp dir
+        dir = tempfile.mkdtemp()
+        local_file = f"{dir}/data"
+        utils.random_binary_file(local_file, 1024)
+
+        # test variations of path
+        source_list = [
+            f"{dir}/data",
+            f"{dir}/*"
+        ]
+
+        dest_list = [
+            # (dest in cp command, expected path of remote file)
+            (f"xet://{user}/{repo}/{b1}", f"xet://{user}/{repo}/{b1}/data"),
+            (f"xet://{user}/{repo}/{b1}/", f"xet://{user}/{repo}/{b1}/data"),
+            (f"xet://{user}/{repo}/{b1}/zz", f"xet://{user}/{repo}/{b1}/zz"),
+        ]
+
+        recursive_list = [
+            False,
+            True,
+        ]
+
+        try:
+            for src in source_list:
+                for dest in dest_list:
+                    for r in recursive_list:
+                        pyxet.cli._root_copy(src, dest[0], "add data", r)
+                        utils.assert_remote_files_exist(dest[1], [dest[1][6:]])
+                        pyxet.PyxetCLI.rm(dest[1])
+        finally:
+            shutil.rmtree(dir)
     finally:
-        pyxet.BranchCLI.delete()
-
-
-    
+        pyxet.BranchCLI.delete(f"xet://{user}/{repo}", b1, True)

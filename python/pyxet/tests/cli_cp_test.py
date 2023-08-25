5bdb41024e398297c063307f4750bd02a1df9aea
Di <di@xetdata.com>
2023-08-26 00:53:17 +0800

    move tests to python

---
+++ python/pyxet/tests/cli_cp_test.py
@@ -0,0 +1,45 @@
+import pyxet
+import utils
+import tempfile
+
+
+def test_single_file_upload():
+    user = utils.test_account_login()
+    repo = utils.test_repo()
+    b1 = utils.new_random_branch_from(f"xet://{user}/{repo}", "main")
+    
+    # generate a random file in a temp dir
+    dir = tempfile.mkdtemp()
+    local_file = f"{dir}/data"
+    utils.random_binary_file(local_file, 1024)
+
+    # test variations of path
+    source_list = [
+        f"{dir}/data",
+        f"{dir}/*"
+    ]
+
+    dest_list = [
+        # (dest in cp command, expected path of remote file)
+        (f"xet://{user}/{repo}/{b1}", f"xet://{user}/{repo}/{b1}/data"),
+        (f"xet://{user}/{repo}/{b1}/", f"xet://{user}/{repo}/{b1}/data"),
+        (f"xet://{user}/{repo}/{b1}/zz", f"xet://{user}/{repo}/{b1}/zz"),
+    ]
+
+    recursive_list = [
+        False,
+        True,
+    ]
+
+    try:
+        for src in source_list:
+            for dest in dest_list:
+                for r in recursive_list:
+                    pyxet.cli._root_copy(src, dest[0], "add data", r)
+                    utils.assert_remote_files_exist(dest[1], [dest[1][6:]])
+                    pyxet.PyxetCLI.rm(dest[1])
+    finally:
+        pyxet.BranchCLI.delete()
+
+
+    
--- python/pyxet/tests/utils.py
+++ python/pyxet/tests/utils.py
@@ -1,4 +1,11 @@
+import fsspec
+import os
 import pytest
+import pyxet
+import random
+import secrets
+import string
+
 
 
 def safe_import(mod_name: str):
@@ -54,6 +61,58 @@ def skip_if_no(package: str):
     return pytest.mark.skipif(
         not safe_import(package), reason=msg
     )
+
+def test_user_info():
+    user = os.getenv('XET_TEST_USER')
+    assert user is not None
+    email = os.getenv('XET_TEST_EMAIL')
+    assert email is not None
+    token = os.getenv('XET_TEST_TOKEN')
+    assert token is not None
+
+    return {
+        "user": user,
+        "email": email,
+        "token": token,
+    }
+
+def test_repo():
+    repo = os.getenv('XET_TEST_REPO')
+    assert repo is not None
+    return repo
+
+def test_account_login():
+    user_info = test_user_info()
+    pyxet.login(user_info['user'], user_info['token'], user_info['email'])
+    return user_info['user']
+
+def random_string(N):
+    return ''.join(secrets.choice(string.ascii_letters + string.digits)
+              for i in range(N))
+
+def random_binary_file(path, size):
+    with open(path, 'wb') as fout:
+        fout.write(os.urandom(size))
+
+def random_text_file(path, size):
+    chars = ''.join([random.choice(string.printable) for i in range(size)])
+    with open(path, 'w') as f:
+        f.write(chars)
+
+# Make a random branch copying src_branch in repo in format xet://[user]/[repo],
+# returns the new branch name
+def new_random_branch_from(repo, src_branch):
+    dest_branch = random_string(20)
+    pyxet.BranchCLI.make(repo, src_branch, dest_branch)
+    return dest_branch
+
+def assert_remote_files_exist(remote, expected):
+    fs = fsspec.filesystem("xet")
+    listing = fs.glob(remote)
+    print(listing)
+    print(expected)
+    assert listing == expected
+
 
 
 class CONSTANTS:

39ac070d188f78614c34105655279b6638679d81
Hoyt Koepke <hoytak@xethub.com>
2024-05-14 12:40:31 -0700

    Update.

--- python/pyxet/develop.sh
+++ python/pyxet/develop.sh
@@ -1,3 +1,7 @@
-source .env/bin/activate
+# Source this file to get going on development.
+
+bash -e ./scripts/setup_env.sh
+
+source venv/bin/activate
 export MACOSX_DEPLOYMENT_TARGET=10.9
 unset CONDA_PREFIX

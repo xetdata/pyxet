68698422cd54787a31045aa3514234813665a5c5
Hoyt Koepke <hoytak@xetdata.com>
2024-06-26 18:25:09 -0700

    Added force to ensure pyinstaller packages boto3. (#148)
    Fixes issue in which boto3 wasn't packaged with xet cli.
    
    Co-authored-by: Hoyt Koepke <hoytak@xethub.com>

--- python/pyxet/scripts/xet_standalone_entry.py
+++ python/pyxet/scripts/xet_standalone_entry.py
@@ -6,6 +6,8 @@ from pyxet.cli import cli
 # Package in s3 dependencies, otherwise these 
 # will just give other repo errors.  
 import s3fs
+import boto3
+import botocore
 
 if __name__ == "__main__":
     cli()
